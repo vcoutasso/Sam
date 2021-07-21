f9d3e4bda43431a91bd8075f307e732b9840f900
Vinícius Couto <vcoutasso@gmail.com>
2021-07-21 19:12:14 -0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+ platform :ios, '14.0'
+
+target 'Sam' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Sam
+  pod 'SwiftFormat/CLI'
+  pod 'SwiftLint'
+
+end
---
+++ Sam/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Sam
+//
+//  Created by Vinícius Couto on 21/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
